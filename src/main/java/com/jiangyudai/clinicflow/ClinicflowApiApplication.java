201be5ccd8ccd99bff125fbd65583e30ecd97244
Jiangyu Dai <dai00055@algonquincollege.com>
2026-09-02 02:56:47 -0400

    chore: initialize Spring Boot application

---
+++ src/main/java/com/jiangyudai/clinicflow/ClinicflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.jiangyudai.clinicflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiangyudai/clinicflow/ClinicflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangyudai.clinicflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
