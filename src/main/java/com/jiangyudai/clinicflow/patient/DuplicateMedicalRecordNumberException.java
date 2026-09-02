78e43214b2720772a7b17709b32c415eafc71504
Jiangyu Dai <dai00055@algonquincollege.com>
2026-09-02 16:45:01 -0400

    feat: add patient REST API with layered package structure

--- src/main/java/com/jiangyudai/clinicflow/patient/DuplicateMedicalRecordNumberException.java
+++
@@ -1,8 +0,0 @@
-package com.jiangyudai.clinicflow.patient;
-
-public class DuplicateMedicalRecordNumberException extends RuntimeException {
-
-    public DuplicateMedicalRecordNumberException(String medicalRecordNumber) {
-        super("Medical record number already exists: " + medicalRecordNumber);
-    }
-}
